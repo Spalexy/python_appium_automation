2746fa7ca5bce980029e8012bbdf4725c11a5554
Alexander Pridacha <a.pridacha@bestdoctor.ru>
2019-06-16 01:51:55 +0300

    Тест класса возвращающего строку

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 class MainClass:
 
     class_number = 20
+    class_string = 'Hello, world'
 
     @staticmethod
     def get_local_number():
@@ -10,3 +11,6 @@ class MainClass:
 
     def get_class_number(self):
         return self.class_number
+
+    def get_class_string(self):
+        return self.class_string
--- test.py
+++ test.py
@@ -6,7 +6,11 @@ from app import MainClass
 class MainClassTest(unittest.TestCase):
 
     def test_get_local_number(self):
-        assert MainClass.get_local_number() == 14, "Функция get_local_number вернула число не равное 14"
+        assert MainClass.get_local_number() == 14, 'Функция get_local_number вернула число не равное 14'
 
     def test_get_class_number(self):
-        assert MainClass.get_class_number(MainClass) > 45, "Номер тестируемого класса <= 45"
+        assert MainClass.get_class_number(MainClass) > 45, 'Номер тестируемого класса <= 45'
+
+    def test_get_class_string(self):
+        class_str = MainClass.get_class_string(MainClass)
+        assert any(s in class_str for s in ['Hello', 'hello']), 'Строка класса не содержит ключевого слова'
