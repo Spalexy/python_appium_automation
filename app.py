0e9521c66b80bb17172bb0b3126455cd675ae4e0
Alexander Pridacha <a.pridacha@bestdoctor.ru>
2019-06-15 15:00:53 +0300

    Функция get_class_number и ее тест

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 
 
 class MainClass:
+
+    class_number = 20
+
     @staticmethod
     def get_local_number():
         return 14
 
+    def get_class_number(self):
+        return self.class_number
--- test.py
+++ test.py
@@ -7,3 +7,6 @@ class MainClassTest(unittest.TestCase):
 
     def test_get_local_number(self):
         assert MainClass.get_local_number() == 14, "Функция get_local_number вернула число не равное 14"
+
+    def test_get_class_number(self):
+        assert MainClass.get_class_number(MainClass) > 45, "Номер тестируемого класса <= 45"
