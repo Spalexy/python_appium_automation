5b92cd373716758884099e29b889268e6b4b05a6
Alexander Pridacha <a.pridacha@bestdoctor.ru>
2019-06-17 11:36:24 +0300

    Исправлен вызов класса

--- test.py
+++ test.py
@@ -6,11 +6,12 @@ from app import MainClass
 class MainClassTest(unittest.TestCase):
 
     def test_get_local_number(self):
-        assert MainClass.get_local_number() == 14, 'Функция get_local_number вернула число не равное 14'
+        assert MainClass().get_local_number() == 14, 'Функция get_local_number вернула число не равное 14'
 
     def test_get_class_number(self):
-        assert MainClass.get_class_number(MainClass) > 45, 'Номер тестируемого класса <= 45'
+        MainClass()
+        assert MainClass().get_class_number() > 45, 'Номер тестируемого класса <= 45'
 
     def test_get_class_string(self):
-        class_str = MainClass.get_class_string(MainClass)
+        class_str = MainClass().get_class_string()
         assert any(s in class_str for s in ['Hello', 'hello']), 'Строка класса не содержит ключевого слова'
